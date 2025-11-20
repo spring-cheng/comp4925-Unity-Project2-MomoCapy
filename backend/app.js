73ff4fcf55e4cd20a11c995195f53e34890e6d9f
Spring Cheng <scheng148@my.bcit.ca>
2025-11-20 11:24:17 -0800

    Change port number

--- backend/app.js
+++ backend/app.js
@@ -7,7 +7,7 @@ import path from 'path';
 import bcrypt from 'bcryptjs';
 import database from './databaseConnectionMongoDB.js';
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
 const app = express();
 
